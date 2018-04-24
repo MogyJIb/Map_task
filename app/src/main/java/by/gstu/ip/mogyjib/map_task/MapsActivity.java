d8b612d7aa0e660264a682b38cf8fd12b1ddc33b
Evgeniy Shevtsov <zhenya_861533@tut.by>
2018-04-24 07:53:39 +0300

    #2 Add current location marker

--- app/src/main/java/by/gstu/ip/mogyjib/map_task/MapsActivity.java
+++ app/src/main/java/by/gstu/ip/mogyjib/map_task/MapsActivity.java
@@ -1,21 +1,52 @@
 package by.gstu.ip.mogyjib.map_task;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import by.gstu.ip.mogyjib.map_task.utils.GoogleApiClientUtil;
+import by.gstu.ip.mogyjib.map_task.utils.LocationUtil;
 
 public class MapsActivity extends AppCompatActivity
-        implements OnMapReadyCallback {
+        implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
 
     private static final String TAG = MapsActivity.class.getSimpleName();
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mLocation;
+    private Marker mMarker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +60,25 @@ public class MapsActivity extends AppCompatActivity
                 (SupportMapFragment) getSupportFragmentManager()
                         .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            LocationUtil.checkLocationPermission(this, this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LocationUtil.MY_LOCATION_PERMITION_CODE) {
+            if (permissions.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                if (mGoogleApiClient == null)
+                    mGoogleApiClient = GoogleApiClientUtil
+                            .buildGoogleApiClient(this, this, this);
+                mMap.setMyLocationEnabled(true);
+            }
+        }
     }
 
     /**
@@ -37,7 +87,7 @@ public class MapsActivity extends AppCompatActivity
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        this.mMap = googleMap;
         try {
             // Customise the styling of the base map using a JSON object defined
             // in a raw resource file.
@@ -51,7 +101,63 @@ public class MapsActivity extends AppCompatActivity
         } catch (Resources.NotFoundException e) {
             Log.e(TAG, "Can't find style. Error: ", e);
         }
-        // Position the map's camera near Sydney, Australia.
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED){
+                mMap.setMyLocationEnabled(true);
+                mGoogleApiClient = GoogleApiClientUtil
+                        .buildGoogleApiClient(this,this,this);
+            }
+        }
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationUtil.getLocationRequest(100,
+                LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            LocationServices.FusedLocationApi
+                    .requestLocationUpdates(mGoogleApiClient,mLocationRequest,this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLocation = location;
+        if(mMarker!=null)
+            mMarker.remove();
+
+        LatLng latLng = LocationUtil.getLatLng(mLocation);
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(latLng)
+                .title("Your position")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        mMarker = mMap.addMarker(markerOptions);
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
+        if(mGoogleApiClient!=null)
+            LocationServices.FusedLocationApi
+                    .removeLocationUpdates(mGoogleApiClient,this);
+
     }
 }
